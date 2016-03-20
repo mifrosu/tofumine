686e4475408611fca068cb739446538c0f27bde8
Michael O'Sullivan <mifrosu@gmail.com>
2016-03-20 12:31:42 +0000

    Add simple Flux

--- tofumine/app/assets/javascripts/application.js
+++ tofumine/app/assets/javascripts/application.js
@@ -13,8 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require lodash
 //= require react
 //= require react_ujs
+//= require flux
+//= require eventemitter
+//= require app
 //= require components
 //= require_tree .
 
